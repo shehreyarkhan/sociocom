bc44494202d22d29e897db64cf6b8e578a6902e8
Shehreyar Khan <Shehreyarkhan35@gmail.com>
2023-07-20 18:35:52 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <section className="flex-start fleex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Loadmore</h1>
+    </section>
+  );
+};
+
+export default Home;
